b2bdd32fc67e05203aee90a8d47a0f15c13f8d54
Mikhail Dementev <dex-max89@mail.ru>
2017-09-25 21:46:19 +0300

    Added messages

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -16,4 +16,32 @@ angular.module('RoutingApp', ['Helper','ngRoute','RegApp','SinginApp','AdminApp'
             .otherwise({
                 redirectTo: '/'
             });
-    }]);
+    }])
+    .controller('IndexCrt', function ($scope, $rootScope) {
+        $scope.alert ={
+            hide: true
+        };
+        $scope.clickAlert = function () {
+            $scope.alert ={
+                hide: true
+            };
+        };
+        $rootScope.alert = function (type, text) {
+            switch (type) {
+                case 1:
+                    $scope.alert = {
+                        hide: false,
+                        class: "alert-success",
+                        text: text
+                    };
+                    break;
+                case 0:
+                    $scope.alert = {
+                        hide: false,
+                        class: "alert-danger",
+                        text: text
+                    };
+                    break
+            }
+        };
+    });
--- public/js/reg/reg.js
+++ public/js/reg/reg.js
@@ -1,18 +1,18 @@
 angular.module('RegApp',[])
     .controller('RegCrt', function ($scope,$rootScope,$http) {
         $scope.reg = function () {
-            /*
-            $http({method: 'GET', url: '/server/index.php'})
-                .then(function success(response) {
-                    alert(response.data);
-                })*/
-            var data = {
-                login: $scope.login,
-                password: $scope.password,
-                email: $scope.email
-            };
-            $http.post('/server/index.php', data).then(function (resp) {
-                console.log(resp.data);
-            })
+            //$rootScope.test();
+            if ($scope.password === $scope.password1 && $scope.email && $scope.password) {
+                var data = {
+                    password: $scope.password,
+                    email: $scope.email
+                };
+                $http.post('/server/index.php', data).then(function (resp) {
+                    $rootScope.alert (1, "Данные получены с сервера!");
+                    console.log(resp.data);
+                })
+            }else {
+                $rootScope.alert (0, "Форма регистрации неверно заполнена!");
+            }
         }
     });
